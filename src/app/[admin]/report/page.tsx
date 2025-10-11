d571bb74283c4408ff9ae595a8a504de54c6a53f
__lleq <ball_090573@hotmail.com>
2025-10-11 16:02:30 +0700

    Update Report Temp

---
+++ src/app/[admin]/report/page.tsx
@@ -0,0 +1,9 @@
+export default function ReportPage(){
+    return(
+        <div className="min-h-screen bg-base-200 p-4">
+            <div className="max-w-7xl mx-auto">
+                <div>Report</div>
+            </div>
+        </div>
+    )
+}
