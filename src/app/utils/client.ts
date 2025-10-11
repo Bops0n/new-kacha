1f1d133b0aa158f85446285a631ed7909db60262
__lleq <ball_090573@hotmail.com>
2025-10-11 16:13:43 +0700

    Update Check Require Access

--- src/app/utils/client.ts
+++ src/app/utils/client.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
 
-export const requireAdmin = (auth : any) => {
+export const checkRequire = (auth : any) => {
     if (!auth.authenticated) {
         return auth.response;
     }
-    if (auth.accessLevel !== 9) {
+    if (auth.accessLevel < 1) {
         return NextResponse.json({ message: 'Forbidden' }, { status: 403 });
     }
     return null;
