31b804d7f3673f861026a7fc239d807bd14c5c9a
__lleq <ball_090573@hotmail.com>
2025-10-15 20:59:01 +0700

    Update Auth API Service

---
+++ src/app/api/services/auth/authService.ts
@@ -0,0 +1,14 @@
+import { hmacMd5Hex } from "@/app/utils/cryptor";
+import { poolQuery } from "../../lib/db";
+
+export async function signIn(username: string, password: string) {
+    const { rows } = await poolQuery(`SELECT * FROM public."SP_AUTH_LOGIN"($1, $2);`, 
+        [username, hmacMd5Hex(password, process.env.SALT_SECRET)]);
+    return rows[0];
+}
+
+export async function signUp(username: string, email: string, password: string) {
+    const { rows } = await poolQuery(`SELECT * FROM "SP_AUTH_REGISTER"($1, $2, $3)`, 
+        [username, email, hmacMd5Hex(password, process.env.SALT_SECRET)]);
+    return rows[0];
+}
